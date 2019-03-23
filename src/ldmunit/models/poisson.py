0af0dc28e4a08452d8507d828af068f0fbca1c9c
wyq977 <wangyq977@gmail.com>
2019-03-23 15:02:47 +0100

    add alternative poisson model
    require log-likelihood calculation expression

--- src/ldmunit/models/poisson.py
+++ src/ldmunit/models/poisson.py
@@ -16,3 +16,15 @@ class PoissonModel(sciunit.Model, ProducesNumber):
 
     def produce_number(self):
         return poisson(self.lam)
+
+class PoissonAltModel(sciunit.Model, capabilities.ProducesLoglikelihood):
+    """A alternative way to represent Poisson model."""
+
+    def __init__(self, para, name=None):
+        self.para = para
+        super(PoissonAltModel, self).__init__(name=name, para=para)
+
+    def produce_loglikelihood(self):
+        return dict(zip(['para', 'lpdf'], 
+        # pdf of poi(para): e^-para * para^x / (x!)
+        [self.para,  lambda para, obs: -para + obs * np.log(para)])) # terms w/ only observation was omitted
