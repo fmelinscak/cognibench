0c81b2b516f256e3b31f76d1af2e49e524cd528a
wyq977 <wangyq977@gmail.com>
2019-05-20 00:41:09 +0200

    change produce_loglikelihood
    add multiple subject support, add a wrapper function for optimization

--- src/ldmunit/models/rw.py
+++ src/ldmunit/models/rw.py
@@ -1,31 +1,47 @@
 import sciunit
 import numpy as np
 from ..capabilities import ProducesLoglikelihood
-from ..utils import softmax
-from numba import jit
+
+template="""
+import numpy as np
+def softmax(x, beta):
+    return np.exp(x * beta) / np.sum(np.exp(x * beta), axis=0)
+def func(variable, stimuli, rewards, actions, {fixed}):
+    {variable} = variable
+    prob_log = 0
+    stimuli_list = np.unique(stimuli).tolist()
+    n_actions = np.unique(actions).shape[0]
+    Q = dict([[stimulus, np.zeros(n_actions)] for stimulus in stimuli_list])
+    for action, reward, stimulus in zip(actions, rewards, stimuli):
+        Q[stimulus][action] += alpha * (reward - Q[stimulus][action])
+        prob_log += np.log(softmax(Q[stimulus], beta)[action])
+    return -prob_log
+"""
 
 class RWModel(sciunit.Model, ProducesLoglikelihood):
     """A decision making models (Rescorla–Wagner model)"""
 
-    def __init__(self, alpha, beta, name=None):
-        self.alpha = alpha
-        self.beta = beta
-        super(RWModel, self).__init__(name=name,
-                                          alpha=alpha, beta=beta)
- 
-    def produce_loglikelihood(self, rewards, stimuli):
+    def __init__(self, paras=None, name=None):
+        # if paras != None:
+        self.paras = paras
+        super().__init__(name=name, paras=paras) # py 3 
 
-        def logpdf(actions): 
-            """Calculate the negative log-likelihood score given actions."""
-            prob_log = 0
-            stimuli_list = np.unique(stimuli).tolist()
-            n_actions = np.unique(actions).shape[0]
+    def __make_func(self, *fixed):
+        """Create a local negative log-likelihood function for optimization"""
+        variable = sorted(set(('alpha', 'beta')).difference(fixed))
+        ns = dict() # define namespace
+        funcstr = template.format(variable=', '.join(variable), fixed=', '.join(fixed))
+        exec(funcstr, ns) # define func in namespace 
+        return ns['func']
 
-            Q = dict([[stimulus, np.zeros(n_actions)] for stimulus in stimuli_list])
-            for action, reward, stimulus in zip(actions, rewards, stimuli):
-                Q[stimulus][action] += self.alpha * (reward - Q[stimulus][action])
-                prob_log += np.log(softmax(Q[stimulus], self.beta)[action])
-
-            return -prob_log
-
+    def produce_loglikelihood(self, stimuli, rewards):
+        #TODO: add error handle
+        assert isinstance(self.paras, list)
+        f = self.__make_func() # negative log-likelihood
+        def logpdf(actions):
+            res = 0
+            for a, r, s, p in zip(actions, rewards, stimuli, self.paras):
+                p_ = list(p.values()) # unpack paras dict
+                res -= f(p_, s, r, a)
+            return res
         return logpdf
