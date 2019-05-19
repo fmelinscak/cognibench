f67532d221910609a14e125783a5efd958211025
wyq977 <wangyq977@gmail.com>
2019-05-20 01:22:32 +0200

    change produce_loglikelihood
    add support for groups of subjects

--- src/ldmunit/models/nwsls.py
+++ src/ldmunit/models/nwsls.py
@@ -4,28 +4,33 @@ from ..capabilities import ProducesLoglikelihood
 
 class NWSLSModel(sciunit.Model, ProducesLoglikelihood):
 
-    def __init__(self, epsilon, name=None):
-        self.epsilon = epsilon
-        super(NWSLSModel, self).__init__(name=name,
-                                          epsilon=epsilon)
- 
-    def produce_loglikelihood(self, rewards, stimuli):
+    def __init__(self, paras, name=None):
+        # if paras != None:
+        self.paras = paras
+        super().__init__(name=name, paras=paras) # py 3 
 
+    @staticmethod
+    def nlll(epsilon, stimuli, rewards, actions):
+        prob_log = 0
+        prob_list = np.array([1-epsilon/2, epsilon/2, epsilon/2, 1-epsilon/2]).reshape((2,2))
+
+        stimuli_list = np.unique(stimuli).tolist()
+        n_actions = np.unique(actions).shape[0]
+        
+        Q = dict([[stimulus, np.zeros(n_actions)] for stimulus in stimuli_list])
+        for action, reward, stimulus in zip(actions, rewards, stimuli):
+            idx = 1 if action == reward else 0
+            Q[stimulus] = prob_list[idx]
+            prob_log += np.log(Q[stimulus][action])
+        return -prob_log
+
+    def produce_loglikelihood(self, stimuli, rewards):
+        #TODO: add error handle
+        assert isinstance(self.paras, list)
         def logpdf(actions):
-
-            prob_log = 0
-            epsilon = self.epsilon
-            prob_list = np.array([1-epsilon/2, epsilon/2, epsilon/2, 1-epsilon/2]).reshape((2,2))
-
-            stimuli_list = np.unique(stimuli).tolist()
-            n_actions = np.unique(actions).shape[0]
-            
-            Q = dict([[stimulus, np.zeros(n_actions)] for stimulus in stimuli_list])
-            for action, reward, stimulus in zip(actions, rewards, stimuli):
-                idx = 1 if action == reward else 0
-                Q[stimulus] = prob_list[idx]
-                prob_log += np.log(Q[stimulus][action])
-            
-            return -prob_log
-
+            res = 0
+            for a, r, s, p in zip(actions, rewards, stimuli, self.paras):
+                p_ = list(p.values()) # unpack paras dict
+                res -= self.nlll(p_[0], s, r, a)
+            return res
         return logpdf
