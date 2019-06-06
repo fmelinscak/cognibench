a4b44871f3f523e3b5b51de87135c4bb1ae420a7
wyq977 <wangyq977@gmail.com>
2019-06-06 11:50:05 +0200

    add multi-sub models for RWCK

--- src/ldmunit/models/RWCK.py
+++ src/ldmunit/models/RWCK.py
@@ -83,3 +83,83 @@ class RWCKModel(sciunit.Model, SupportsDiscreteActions):
     def loglikelihood(self, P, action):
         """Return log-likelihood value of action based on P given by predict() method"""
         return P[action]
+
+class RWCKMultiModel(sciunit.Model, SupportsDiscreteActions):
+    """Rescorla Wagner Choice kernel Model for discrete decision marking."""
+    def __init__(self, n_actions, n_obs, paras=None, name=None):
+        assert isinstance(n_actions, int)
+        assert isinstance(n_obs, int)
+        self.paras = paras
+        self.name = name
+        self.n_actions = n_actions
+        self.n_obs = n_obs
+        self.action_space = spaces.Discrete(self.n_actions)
+        self.observation_space = spaces.Discrete(self.n_obs)
+
+        self.n_sub = len(self.paras)
+
+        # init model's state #TODO: add init w0 in 'Q'
+        hidden_state = {'CK': dict([[i, np.zeros(n_actions)] for i in range(n_obs)]),
+                        'Q' : dict([[i, np.zeros(n_actions)] for i in range(n_obs)])}
+
+        self.hidden_state = [hidden_state] * self.n_sub
+
+    def predict(self, stimulus, sub):
+        """Predict choice probabilities based on stimulus (observation in AI Gym)."""
+        assert self.observation_space.contains(stimulus)
+        assert self.paras != None #TODO: add assert for keys
+        # get model's state
+        CK, Q = self.hidden_state[sub]['CK'][stimulus], self.hidden_state[sub]['Q'][stimulus]
+        
+        # unpack parameters
+        beta   = self.paras[sub]['beta']
+        beta_c = self.paras[sub]['beta_c' ]
+
+        V = beta * Q + beta_c * CK
+        P = softmax(V, 1)
+
+        return P
+
+    def update(self, stimulus, reward, action, done, sub): #TODO: add default value
+        """Update model's state given stimulus (observation in AI Gym), reward, action in the environment."""
+        assert self.action_space.contains(action)
+        assert self.observation_space.contains(stimulus)
+        assert self.paras != None #TODO: add assert for keys
+
+        # get model's state
+        CK, Q = self.hidden_state[sub]['CK'][stimulus], self.hidden_state[sub]['Q'][stimulus]
+        
+        if not done:
+            # unpack parameters
+            alpha   = self.paras[sub]['alpha'  ]
+            alpha_c = self.paras[sub]['alpha_c']
+
+            # update choice kernel
+            CK = (1 - alpha_c) * CK
+            CK[action] += alpha_c * reward
+
+            # update Q weights
+            delta = reward - Q[action]
+            Q[action] += alpha * delta
+
+            self.hidden_state[sub]['CK'][stimulus] = CK
+            self.hidden_state[sub]['Q' ][stimulus] = Q
+
+        return CK, Q
+
+    def reset(self):
+        """Reset model's state."""
+        hidden_state = {'CK': dict([[i, np.zeros(self.n_actions)] for i in range(self.n_obs)]),
+                        'Q' : dict([[i, np.zeros(self.n_actions)] for i in range(self.n_obs)])}
+
+        self.hidden_state = [hidden_state] * self.n_sub
+        return self.action_space.sample()
+    
+    def act(self, p):
+        """Agent make decision/choice based on the probabilities."""
+        assert len(p) == self.n_actions
+        return np.random.choice(range(self.n_actions), p=p)
+
+    def loglikelihood(self, P, action):
+        """Return log-likelihood value of action based on P given by predict() method"""
+        return P[action]
