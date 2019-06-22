b8719c25596e009de0017aa943bd1b62465add00
wyq977 <wangyq977@gmail.com>
2019-06-22 10:36:09 +0200

    update bb

--- src/ldmunit/models/beta_binomial.py
+++ src/ldmunit/models/beta_binomial.py
@@ -3,11 +3,12 @@ import numpy as np
 from gym import spaces
 from scipy.optimize import minimize
 from scipy.stats import beta
+from ..capabilities import Interactive
 
-class BetaBinomialModel(sciunit.Model):
+class BetaBinomialModel(sciunit.Model, Interactive):
 
-    action_space = spaces.Box(-1000, 1000, shape=(1,), dtype=np.float32) #TODO: change
-    observation_space = spaces.MultiBinary(2)
+    action_space = spaces.Box
+    observation_space = spaces.MultiBinary
 
     def __init__(self, n_obs, paras=None, name=None):
         self.paras = paras
@@ -25,27 +26,16 @@ class BetaBinomialModel(sciunit.Model):
         BetaBinomialModel.action_space = spaces.Box(-1000, 1000, shape=(1,), dtype=np.float32)
         BetaBinomialModel.observation_space = spaces.MultiBinary(n_obs)
 
-    def predict(self, paras, stimulus):
-        """Predict choice probabilities based on stimulus (observation in AI Gym)."""
+    def predict(self, stimulus):
         assert BetaBinomialModel.observation_space.contains(stimulus)
-
-        # unpack parameters
-        slope     = self.paras['slope']
-        mixCoef   = self.paras['mixCoef']
-        intercept = self.paras['intercept']
-
         # get model's state
         a = self.hidden_state['a']
         b = self.hidden_state['b']
 
-        # Generate outcome prediction
-        mu      = beta.mean(a, b)
-        entropy = beta.entropy(a, b)
-
-        return intercept + np.dot(stimulus, (mixCoef * mu  + (1 - mixCoef) * entropy)) * slope
+        logpdf = lambda x: beta.logpdf(x, a=a, b=b)
+        return logpdf
 
     def update(self, stimulus, reward, action, done): #TODO: add default value
-        """Update model's state given stimulus (observation in AI Gym), reward, action in the environment."""
         assert self.observation_space.contains(stimulus)
         # get model's state
         a = self.hidden_state['a']
@@ -60,9 +50,21 @@ class BetaBinomialModel(sciunit.Model):
         return a, b
 
     def reset(self):
-        """Reset model's state."""
         self.hidden_state = self._set_hidden_state(self.n_obs, self.params)      
         return None
     
-    def act(self):
-        pass
+    def act(self, stimulus):
+        """observation function"""
+        mixCoef = self.paras['mixCoef']
+        b0 = self.paras['b0'] # intercept
+        b1 = self.paras['b1'] # slope #TODO: add variable slopes
+
+        # get model's state
+        a = self.hidden_state['a']
+        b = self.hidden_state['b']
+
+        # Generate outcome prediction
+        mu      = beta.mean(a, b)
+        entropy = beta.entropy(a, b)
+        
+        return b0 + np.dot(stimulus, (mixCoef * mu  + (1 - mixCoef) * entropy)) * b1
