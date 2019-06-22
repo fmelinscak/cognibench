2841ecf89037a23248fef466d6877bc93a002622
wyq977 <wangyq977@gmail.com>
2019-06-22 10:35:34 +0200

    update capability

--- src/ldmunit/capabilities.py
+++ src/ldmunit/capabilities.py
@@ -4,14 +4,32 @@ import sciunit
 ### The Capabilities ###
 
 
-class ProducesLoglikelihood(sciunit.Capability):
+class Interactive(sciunit.Capability):
+    """
+    Description:
+        Requires models to respound to the environment in a interactive manner.
 
-    def produce_loglikelihood(self):
-        """Produces the loglikelihood function for agent's actions."""
-        raise NotImplementedError("Must implement produce_loglikelihood.")
+    Observation: 
+        Type: defined in each model.
+        
+    Actions:
+        Type: defined in each model.
+        
+    Reward:
+        In the decision making model setting, reward is 1 when correct action is chosen.
+    """
 
-class Trainable(sciunit.Capability):
+    def predict(self):
+        """Return pmf/pdf based on stimulus (observation in AI Gym)."""
+        raise NotImplementedError("Must implement predict.")
+    
+    def reset(self):
+        """Update model's state given stimulus (observation in AI Gym), reward, action in the environment."""
+        raise NotImplementedError("Must implement reset.")
 
-    def train_with_observations(self):
-        """Fit the parameters of the models."""
-        raise NotImplementedError("Must implement train_with_observations.")
+    def update(self):
+        """Update model's state given stimulus (observation in AI Gym), reward, action in the environment."""
+        raise NotImplementedError("Must implement update.")
+
+    def act(self):
+        raise NotImplementedError("Must implement act")
