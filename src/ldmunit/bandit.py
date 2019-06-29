550303837bca23c26624029c1adad007d049344e
wyq977 <wangyq977@gmail.com>
2019-06-30 00:19:39 +0200

    add bandit

--- src/ldmunit/bandit.py
+++ src/ldmunit/bandit.py
@@ -2,7 +2,7 @@ import numpy as np
 import gym
 from gym import spaces
 from gym.utils import seeding
-
+from .continous import Continous
 
 class BanditEnv(gym.Env):
     """
@@ -11,35 +11,25 @@ class BanditEnv(gym.Env):
 
     p_dist:
         A list of probabilities of the likelihood that a particular bandit will pay out
-    r_dist:
-        A list of either rewards (if number) or means and standard deviations (if list)
-        of the payout that bandit has
     info:
         Info about the environment that the agents is not supposed to know. For instance,
         info can releal the index of the optimal arm, or the value of prior parameter.
         Can be useful to evaluate the agent's perfomance
     """
-    def __init__(self, p_dist, r_dist, info={}):
-        if len(p_dist) != len(r_dist):
-            raise ValueError("Probability and Reward distribution must be the same length")
-
+    def __init__(self, p_dist, info={}):
         if min(p_dist) < 0 or max(p_dist) > 1:
             raise ValueError("All probabilities must be between 0 and 1")
 
-        for reward in r_dist:
-            if isinstance(reward, list) and reward[1] <= 0:
-                raise ValueError("Standard deviation in rewards must all be greater than 0")
-
         self.p_dist = p_dist
-        self.r_dist = r_dist
 
         self.info = info
 
         self.n_bandits = len(p_dist)
         self.action_space = spaces.Discrete(self.n_bandits)
-        # self.observation_space = spaces.box.Box(-1.0, 1.0, (1)) #
         self.observation_space = spaces.Discrete(1)
+        self.action_space = spaces.Box(-1000, 1000, shape=(1,), dtype=np.float32)
 
+        
         self.seed()
 
     def seed(self, seed=None):
@@ -47,43 +37,33 @@ class BanditEnv(gym.Env):
         return [seed]
 
     def step(self, action):
-        assert self.action_space.contains(action)
 
         reward = 0
-        done = False #True
+        done = False
 
         if np.random.uniform() < self.p_dist[action]:
-            if not isinstance(self.r_dist[action], list):
-                reward = self.r_dist[action]
-            else:
-                reward = np.random.normal(self.r_dist[action][0], self.r_dist[action][1])
+            reward = 1
 
-        return 0, reward, done, self.info # [0]
+        return 0, reward, done, self.info
 
     def reset(self):
-        return 0 #[0]
+        return np.random.randint(0, self.n_bandits)
 
     def render(self, mode='human', close=False):
         pass
 
-class BanditAssociateEnv(gym.Env):
+class BanditAssociateEnv(BanditEnv):
     def __init__(self, p_dist, r_dist, info={}):
 
         if min(p_dist) < 0 or max(p_dist) > 1:
             raise ValueError("All probabilities must be between 0 and 1")
 
-        for reward in r_dist:
-            if isinstance(reward, list) and reward[1] <= 0:
-                raise ValueError("Standard deviation in rewards must all be greater than 0")
-
         self.p_dist = p_dist
-        self.r_dist = r_dist
-
         self.info = info
 
         self.n_bandits = len(p_dist)
         self.observation_space = spaces.MultiBinary(self.n_bandits)
-        self.action_space = spaces.Box(-1000, 1000, shape=(1,), dtype=np.float32)
+        self.action_space = Continous
 
         self.seed()
 
@@ -95,12 +75,12 @@ class BanditAssociateEnv(gym.Env):
         assert self.action_space.contains(action)
 
         reward = 0
-        done = False #True
+        done = False
 
-        reward = np.random.normal(self.r_dist[0], self.r_dist[1])
+        reward = np.random.randint(0, 1)
         observation = self.observation_space.sample()
 
-        return observation, reward, done, self.info # [0]
+        return observation, reward, done, self.info
 
     def reset(self):
         return self.observation_space.sample()
