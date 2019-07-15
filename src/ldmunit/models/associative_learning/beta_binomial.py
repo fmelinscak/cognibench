c92242f8a17d99ddbc5e9767857d11a9c34ee0d7
Esref Ozdemir <esref.ozdemir27@gmail.com>
2019-07-15 17:15:07 +0200

    Remove NpDict since subclassing dict is generally bad

--- src/ldmunit/models/associative_learning/beta_binomial.py
+++ src/ldmunit/models/associative_learning/beta_binomial.py
@@ -6,17 +6,6 @@ from scipy import stats
 from scipy.stats import beta
 from .base import CAMO
 
-class NpDict(dict):
-    "Automatically set np.ndarray to tuple for key access."
-    def __getitem__(self, key):
-        key = tuple(key) if isinstance(key, np.ndarray) else key
-        val = dict.__getitem__(self, key)
-        return val
-
-    def __setitem__(self, key, val):
-        key = tuple(key) if isinstance(key, np.ndarray) else key
-        dict.__setitem__(self, key, val)
-
 class BetaBinomialModel(CAMO):
     name = 'BetaBinomial'
 
@@ -31,7 +20,7 @@ class BetaBinomialModel(CAMO):
 
     def reset(self, paras=None):
         assert isinstance(self.n_obs, int), "observation space must be set"
-        self.hidden_state = NpDict()
+        self.hidden_state = dict()
 
     def observation(self, stimulus, paras=None):
         if not paras:
@@ -53,29 +42,37 @@ class BetaBinomialModel(CAMO):
 
     def predict(self, stimulus, paras=None):
         if tuple(stimulus) not in self.hidden_state.keys():
-            self.hidden_state[stimulus] = self._set_a_b()
+            self._set_hidden_state(stimulus, self._set_a_b())
         return self.observation(stimulus, paras).logpdf
 
     def act(self, stimulus, paras=None):
         if tuple(stimulus) not in self.hidden_state.keys():
-            self.hidden_state[stimulus] = self._set_a_b()
+            self._set_hidden_state(stimulus, self._set_a_b())
         return self.observation(stimulus, paras).rvs()
 
     def update(self, stimulus, reward, action, done, paras=None):
         assert self.observation_space.contains(stimulus)
         # get model's state
         if tuple(stimulus) not in self.hidden_state.keys():
-            self.hidden_state[stimulus] = self._set_a_b()
-        a = self.hidden_state[stimulus]['a']
-        b = self.hidden_state[stimulus]['b']
+            self._set_hidden_state(stimulus, self._set_a_b())
+        a = self._get_hidden_state(stimulus)['a']
+        b = self._get_hidden_state(stimulus)['b']
 
         if not done:
             a += (1 - stimulus) * (1 - reward) + stimulus * reward
             b += (1 - stimulus) * reward + stimulus * (1 - reward)
-            self.hidden_state[stimulus]['a'] = a
-            self.hidden_state[stimulus]['b'] = b
+            old = self._get_hidden_state(stimulus)
+            old['a'] = a
+            old['b'] = b
+            self._set_hidden_state(stimulus, old)
 
         return a, b
+
+    def _set_hidden_state(self, key, val):
+        self.hidden_state[tuple(key)] = val
+
+    def _get_hidden_state(self, key):
+        return self.hidden_state[tuple(key)]
 
     def _predict_reward(self, stimulus, paras=None):
         if not paras:
@@ -85,9 +82,9 @@ class BetaBinomialModel(CAMO):
         b1 = paras['b1'] # slope
 
         if tuple(stimulus) not in self.hidden_state.keys():
-            self.hidden_state[stimulus] = self._set_a_b()
-        a = self.hidden_state[stimulus]['a']
-        b = self.hidden_state[stimulus]['b']
+            self._set_hidden_state(stimulus, self._set_a_b())
+        a = self._get_hidden_state(stimulus)['a']
+        b = self._get_hidden_state(stimulus)['b']
 
         mu      = beta(a,b).mean()
         entropy = beta(a,b).entropy()
