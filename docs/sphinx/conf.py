39fe7acf1eb04050b7d57971da832d7e506d7e91
Esref Ozdemir <esref.ozdemir27@gmail.com>
2019-07-18 12:13:22 +0200

    Move sphinx folder under docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
 
 
 # -- Project information -----------------------------------------------------
