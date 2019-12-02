dc206d7c16a51400ffaf0d8fd6715dbaa8c07d4e
Eshref Yozdemir <esref.ozdemir27@gmail.com>
2019-12-02 09:32:51 +0100

    NLL, AIC and BIC testing of hbayesdm bandit2arm_delta

--- examples/hbayesdm/model_defs.py
+++ examples/hbayesdm/model_defs.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 from scipy.stats import rv_discrete
 from ldmunit.models import DADO
-from ldmunit.capabilities import BatchTrainable, PredictsLogpdf
+from ldmunit.capabilities import BatchTrainable, PredictsLogpdf, ReturnsNumParams
 
 
-class HbayesdmModel(DADO, PredictsLogpdf, BatchTrainable):
+class HbayesdmModel(DADO, PredictsLogpdf, BatchTrainable, ReturnsNumParams):
     name = "hBayesDM Model"
 
     def __init__(self, *args, hbayesdm_model_func, col_names, **kwargs):
@@ -17,11 +17,18 @@ class HbayesdmModel(DADO, PredictsLogpdf, BatchTrainable):
         self.col_names = col_names
         self.kwargs = kwargs
 
+    def n_params(self):
+        try:
+            return self.n_hbayesdm_paras
+        except AttributeError:
+            print("self.n_hbayesdm_paras not defined; try fitting the model first.")
+
     def fit(self, stimuli, actions):
         df = pd.DataFrame(data=stimuli, columns=self.col_names)
         hbayesdm_out = self.hbayesdm_model_func(
             data=df, inc_postpred=True, **self.kwargs
         )
+        self.n_hbayesdm_paras = len(hbayesdm_out.parameters)
         best_sample_id = np.argmax(hbayesdm_out.par_vals["log_lik"].mean(axis=1))
         self.y_dist = hbayesdm_out.par_vals["y_dist"][best_sample_id]
 
--- examples/hbayesdm/run_tests.py
+++ examples/hbayesdm/run_tests.py
@@ -16,6 +16,19 @@ DATA_PATH = "data"
 sciunit.settings["CWD"] = getcwd()
 
 
+NLLScore = partialclass(scores.NLLScore, min_score=0, max_score=1e4)
+AICScore = partialclass(scores.AICScore, min_score=0, max_score=1e4)
+BICScore = partialclass(scores.BICScore, min_score=0, max_score=1e4)
+
+
+def aic_kwargs_fn(model, obs, pred):
+    return {"n_model_params": model.n_params()}
+
+
+def bic_kwargs_fn(model, obs, pred):
+    return {"n_model_params": model.n_params(), "n_samples": len(obs["stimuli"])}
+
+
 def main():
     df = pd.read_csv("data/bandit2arm_exampleData.txt", delimiter="\t")
     obs = dict()
@@ -29,11 +42,31 @@ def main():
     suite = sciunit.TestSuite(
         [
             BatchTrainAndTest(
-                name="Accuracy Test",
+                name="NLL Test",
                 observation=obs,
                 train_indices=train_indices,
                 test_indices=test_indices,
-                score_type=partialclass(scores.NLLScore, min_score=0, max_score=1e4),
+                score_type=NLLScore,
+                persist_path="logs",
+                logging=2,
+            ),
+            BatchTrainAndTest(
+                name="AIC Test",
+                observation=obs,
+                train_indices=train_indices,
+                test_indices=test_indices,
+                score_type=AICScore,
+                fn_kwargs_for_score=aic_kwargs_fn,
+                persist_path="logs",
+                logging=2,
+            ),
+            BatchTrainAndTest(
+                name="BIC Test",
+                observation=obs,
+                train_indices=train_indices,
+                test_indices=test_indices,
+                score_type=BICScore,
+                fn_kwargs_for_score=bic_kwargs_fn,
                 persist_path="logs",
                 logging=2,
             ),
