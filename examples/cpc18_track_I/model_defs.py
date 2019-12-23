1ecac4f9f8fbe1d69cae3fbee0c4838bed2a094f
Eshref Yozdemir <eshrefyoz@gmail.com>
2019-12-23 14:08:36 +0100

    Adjust CPC-I models to new definitions

--- examples/cpc18_track_I/model_defs.py
+++ examples/cpc18_track_I/model_defs.py
@@ -1,15 +1,19 @@
 from importlib import import_module
-from ldmunit.models import CACO
+from ldmunit.models import LDMModel
+from ldmunit.capabilities import ContinuousAction, ContinuousObservation
+from ldmunit.continuous import ContinuousSpace
 from ldmunit.models.wrappers import (
     OctaveWrapperMixin,
     RWrapperMixin,
 )
 
 
-class BEASTsdPython(CACO):
+class BEASTsdPython(LDMModel, ContinuousAction, ContinuousObservation):
     name = "BEASTsdPython"
 
     def __init__(self, *args, import_base_path, **kwargs):
+        self.set_action_space(ContinuousSpace())
+        self.set_observation_space(ContinuousSpace())
         fn_name = "CPC18_BEASTsd_pred"
         module = import_module(f"{import_base_path}.{fn_name}")
         self.pred_fn = getattr(module, fn_name)
@@ -19,25 +23,31 @@ class BEASTsdPython(CACO):
         return self.pred_fn(*args, **kwargs)
 
 
-class BEASTsdOctave(OctaveWrapperMixin, CACO):
+class BEASTsdOctave(
+    OctaveWrapperMixin, LDMModel, ContinuousAction, ContinuousObservation
+):
     name = "BEASTsdOctave"
 
     def __init__(self, *args, import_base_path, **kwargs):
+        self.set_action_space(ContinuousSpace())
+        self.set_observation_space(ContinuousSpace())
         OctaveWrapperMixin.__init__(
             self,
             import_base_path=import_base_path,
             reset_fn=lambda oct_sess: oct_sess.eval("pkg load statistics;"),
             predict_fn="CPC18_BEASTsd_pred",
         )
-        CACO.__init__(self, *args, **kwargs)
+        LDMModel.__init__(self, *args, **kwargs)
         self.reset()
 
 
-class BEASTsdR(RWrapperMixin, CACO):
+class BEASTsdR(RWrapperMixin, LDMModel, ContinuousAction, ContinuousObservation):
     name = "BEASTsdR"
 
     def __init__(self, *args, import_base_path, **kwargs):
+        self.set_action_space(ContinuousSpace())
+        self.set_observation_space(ContinuousSpace())
         RWrapperMixin.__init__(
             self, import_base_path=import_base_path, predict_fn="CPC18_BEASTsd_pred"
         )
-        CACO.__init__(self, *args, **kwargs)
+        LDMModel.__init__(self, *args, **kwargs)
