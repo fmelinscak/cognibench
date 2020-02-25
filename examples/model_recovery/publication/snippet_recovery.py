c48ef6e003181653b0780a872f699cbc4f821936
Eshref Yozdemir <eshrefyoz@gmail.com>
2020-02-25 16:16:30 +0100

    Change model seeding and sample length in BIC func

--- examples/model_recovery/publication/snippet_recovery.py
+++ examples/model_recovery/publication/snippet_recovery.py
@@ -14,7 +14,7 @@ sciunit.settings["CWD"] = getcwd()
 
 
 def bic_kwargs_fn(model, obs, pred):
-    return {"n_model_params": model.n_params(), "n_samples": len(obs)}
+    return {"n_model_params": model.n_params(), "n_samples": len(obs["stimuli"])}
 
 
 def sm_to_numpy(sm):
@@ -27,45 +27,46 @@ def sm_to_numpy(sm):
 
 
 def do_one(seed):
+    init_seed = seed + int(1e5)
+    env_seed = seed + int(1e6)
+    rng = np.random.RandomState(init_seed)
     n_action, n_obs = 2, 2
 
-    rr = decision_models.RandomRespondModel(n_action=n_action, n_obs=n_obs)
+    rr = decision_models.RandomRespondModel(n_action=n_action, n_obs=n_obs, seed=seed)
     rr.init_paras()
-    rr.set_paras_kw(bias=stats.uniform.rvs())
+    rr.set_paras_kw(bias=rng.uniform())
 
-    nwsls = decision_models.NWSLSModel(n_action=n_action, n_obs=n_obs)
+    nwsls = decision_models.NWSLSModel(n_action=n_action, n_obs=n_obs, seed=seed)
     nwsls.init_paras()
-    nwsls.set_paras_kw(epsilon=stats.uniform.rvs())
+    nwsls.set_paras_kw(epsilon=rng.uniform())
 
-    rw = decision_models.RWModel(n_action=n_action, n_obs=n_obs)
+    rw = decision_models.RWModel(n_action=n_action, n_obs=n_obs, seed=seed)
     rw.init_paras()
-    rw.set_paras_kw(eta=stats.uniform.rvs(), beta=1 + stats.expon.rvs(scale=1))
+    rw.set_paras_kw(eta=rng.uniform(), beta=1 + rng.exponential(scale=1))
 
-    ck = decision_models.CKModel(n_action=n_action, n_obs=n_obs)
+    ck = decision_models.CKModel(n_action=n_action, n_obs=n_obs, seed=seed)
     ck.init_paras()
-    ck.set_paras_kw(eta_c=stats.uniform.rvs(), beta_c=1 + stats.expon.rvs(scale=1))
+    ck.set_paras_kw(eta_c=rng.uniform(), beta_c=1 + rng.exponential(scale=1))
 
-    rwck = decision_models.RWCKModel(n_action=n_action, n_obs=n_obs)
+    rwck = decision_models.RWCKModel(n_action=n_action, n_obs=n_obs, seed=seed)
     rwck.init_paras()
     rwck.set_paras_kw(
-        eta=stats.uniform.rvs(),
-        eta_c=stats.uniform.rvs(),
-        beta=1 + stats.expon.rvs(scale=1),
-        beta_c=1 + stats.expon.rvs(scale=1),
+        eta=rng.uniform(),
+        eta_c=rng.uniform(),
+        beta=1 + rng.exponential(scale=1),
+        beta_c=1 + rng.exponential(scale=1),
     )
 
     model_list = [rr, nwsls, rw, ck, rwck]
 
-    env = BanditEnv(p_dist=[0.2, 0.8])
+    env = BanditEnv(p_dist=[0.2, 0.8], seed=env_seed)
     test_class = partialclass(
         InteractiveTest,
         score_type=partialclass(BICScore, min_score=0, max_score=1000),
         fn_kwargs_for_score=bic_kwargs_fn,
     )
 
-    _, score_matrix = model_recovery(
-        model_list, env, test_class, n_trials=1000, seed=seed
-    )
+    _, score_matrix = model_recovery(model_list, env, test_class, n_trials=1000)
     sm_arr = sm_to_numpy(score_matrix)
     np.save(f"{seed}", sm_arr)
     min_indices = np.argmin(sm_arr, axis=0)
@@ -73,7 +74,7 @@ def do_one(seed):
 
 
 seeds = range(1, 101)
-with multiprocessing.Pool(20) as p:
+with multiprocessing.Pool(34) as p:
     index_list = p.map(do_one, seeds)
 
 cm = np.zeros(shape=(5, 5))
