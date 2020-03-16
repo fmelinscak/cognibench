eb19e15b4f37030895633ec3246ddcccb9b2a8a3
Eshref Yozdemir <eshrefyoz@gmail.com>
2020-03-16 15:02:57 +0100

    Test fss6b and vc7b in one suite

--- examples/pspm_pupil/pipelines/fit.m
+++ examples/pspm_pupil/pipelines/fit.m
@@ -1,7 +1,5 @@
 function out = fit(datapath, subject_id, bf, foreshortening, preprocessing)
-    pupil_fpath = fullfile(datapath, sprintf('FSS6B_pupil_%.2d.mat', subject_id));
-    psych_fpath = fullfile(datapath, sprintf('FSS6B_psych_%.2d.mat', subject_id));
-    [~, conditions] = read_data(pupil_fpath, psych_fpath);
+    [pupil_fpath, conditions] = get_conditions(datapath, subject_id);
 
     model.modelfile = fullfile(datapath, sprintf('glm_%d.mat', subject_id));
     model.datafile = pupil_fpath;
@@ -13,4 +11,41 @@ function out = fit(datapath, subject_id, bf, foreshortening, preprocessing)
     options.overwrite = true;
 
     out = pspm_glm(model, options);
+end
+
+function [pupil_fpath, conditions] = get_conditions(datapath, subject_id)
+    fparts = split(datapath, filesep);
+    if isempty(fparts{end})
+        dataset_name = fparts{end-1};
+    else
+        dataset_name = fparts{end};
+    end
+    func = str2func(sprintf('get_conditions_%s', lower(dataset_name)));
+    [pupil_fpath, conditions] = func(datapath, subject_id);
+end
+
+function [pupil_fpath, conditions] = get_conditions_fss6b(datapath, subject_id)
+    pupil_fpath = fullfile(datapath, sprintf('FSS6B_pupil_%.2d.mat', subject_id));
+    psych_fpath = fullfile(datapath, sprintf('FSS6B_psych_%.2d.mat', subject_id));
+    pupil = load(pupil_fpath);
+    psych = load(psych_fpath);
+
+    conditions = struct();
+    csp_markers = psych.data(:, 3) == 1;
+    csn_markers = psych.data(:, 3) == 2;
+    conditions.names = {'CS+', 'CS-'};
+    conditions.onsets = {pupil.data{1}.data(csp_markers), pupil.data{1}.data(csn_markers)};
+end
+
+function [pupil_fpath, conditions] = get_conditions_vc7b(datapath, subject_id)
+    pupil_fpath = fullfile(datapath, sprintf('VC7B_pupil_%.2d_sn1.mat', subject_id));
+    psych_fpath = fullfile(datapath, sprintf('VC7B_psych_%.2d.mat', subject_id));
+    pupil = load(pupil_fpath);
+    psych = load(psych_fpath);
+
+    conditions = struct();
+    csp_markers = psych.data(:, 3) == 1;
+    csn_markers = psych.data(:, 3) == 2;
+    conditions.names = {'CS+', 'CS-'};
+    conditions.onsets = {pupil.data{1}.data(csp_markers), pupil.data{1}.data(csn_markers)};
 end
--- examples/pspm_pupil/test_pspm_pipelines.py
+++ examples/pspm_pupil/test_pspm_pipelines.py
@@ -1,4 +1,5 @@
 from os import getcwd
+from collections import namedtuple
 import numpy as np
 from os.path import join as pathjoin
 from cognibench.testing.tests import BatchTest
@@ -16,19 +17,23 @@ DATA_PATH = "data"
 MODEL_PATH = "pipelines"
 PSPM_PATH = "/home/eozd/bachlab/pspm/src"
 
-if __name__ == "__main__":
-    # prepare data
-    obs_dict0 = {
+Dataset = namedtuple("Dataset", "name subject_ids")
+dataset_list = [
+    Dataset(name="fss6b", subject_ids=np.arange(1, 19)),
+    Dataset(name="vc7b", subject_ids=np.arange(1, 22)),
+]
+obs_dict_list = [
+    {
         "stimuli": {
-            "datapath": pathjoin(DATA_PATH, "fss6b"),
-            "subject_ids": np.arange(1, 19),
+            "datapath": pathjoin(DATA_PATH, ds.name),
+            "subject_ids": ds.subject_ids,
         },
         "actions": [],
     }
-    # obs_dict1 = {"stimuli": pathjoin(DATA_PATH, "PIT2e2.asc"), "actions": actions}
-    # obs_dict2 = {"stimuli": pathjoin(DATA_PATH, "PIT2e3.asc"), "actions": actions}
-    # obs_dict3 = {"stimuli": pathjoin(DATA_PATH, "PIT2e38.asc"), "actions": actions}
+    for ds in dataset_list
+]
 
+if __name__ == "__main__":
     # prepare models
     model_names = [f"model{i}" for i in range(6)]
     models = [
@@ -47,13 +52,14 @@ if __name__ == "__main__":
     suite = TestSuite(
         [
             BatchTest(
-                name="FSS6B Test",
-                observation=obs_dict0,
+                name=f"{ds.name} Test",
+                observation=obs_dict,
                 score_type=CohensD,
-                persist_path=persist_path_fmt.format("FSS6B"),
+                persist_path=persist_path_fmt.format(ds.name),
                 optimize_models=False,
                 logging=2,
             )
+            for obs_dict, ds in zip(obs_dict_list, dataset_list)
         ],
         name="Batch test suite",
     )
