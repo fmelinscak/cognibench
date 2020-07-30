89f36f3da8bca94a7ce1db7f9131f43e610da75b
Filip Melinscak <filip.melinscak@gmail.com>
2020-07-30 09:12:40 +0200

    Remove seed from `simulate` invocation in tests
    Since the model/agent and the environment are initialized in the seed, they are no longer used in the simulate function, but this was not yet propagated to the test.

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -15,7 +15,7 @@ class Test_Unit(unittest.TestCase):
         self.agent = decision_making.RWCKAgent(n_obs=1, n_action=2, paras_dict=paras)
 
     def test_simulation(self):
-        stimuli, rewards, actions = simulate(self.env, self.agent, 100, seed=42)
+        stimuli, rewards, actions = simulate(self.env, self.agent, 100)
         self.assertEqual(self.agent.eval_policy(0).pk[1], 0.8757028699917109)
         self.assertEqual(np.unique(actions, return_counts=True)[1][1], 87)
 
